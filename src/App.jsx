7b03a33edb7cbf17e94f12521ba386d7868ddba4
Jaro111 <jarokark@protonmail.com>
2025-03-27 18:57:29 +0000

    chore: routes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState } from "react";
+import { Navbar } from "./pages/Navbar";
+import { CalendarPage } from "./pages/CalendarPage";
+import { WeekPlanPage } from "./pages/WeekPlanPage";
+import { TrainingPage } from "./pages/TrainingPage";
+import { SchedulePage } from "./pages/SchedulePage";
+import { Footer } from "./pages/Footer";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p>Pipa</p>
-    </>
+    <BrowserRouter basename="">
+      <Navbar />
+      <Routes>
+        <Route path="" element={<CalendarPage />} />
+        <Route path="/trainingPage" element={<TrainingPage />} />
+        <Route path="/weekPlanPage" element={<WeekPlanPage />} />
+        <Route path="/schedulePage" element={<SchedulePage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 

b451444d1a9db02365cd00b52d96e6b676bcb1fd
Jaro111 <jarokark@protonmail.com>
2025-04-02 10:52:14 +0100

    chore: basicly styled and basic funcionality

--- src/components/calendar/DaysPanel.jsx
+++ src/components/calendar/DaysPanel.jsx
@@ -1,42 +1,73 @@
 import React from "react";
+import { weekDays } from "../../data/monthsWeekdays.js";
+import { months } from "../../data/monthsWeekdays.js";
 import { useEffect, useState } from "react";
 import "./DaysPanel.css";
 
 export const DaysPanel = (props) => {
   const [daysArray, setDaysArray] = useState([]);
-  const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const [tempDay, setTempDay] = useState(null);
 
-  const createDaysArray = () => {
+  // get number of the week day number providing a day.
+  const getDayOftheWeek = (day) => {
+    const newDate = new Date(
+      `${months[props.calendarMonth]} ${day}, ${props.calendarYear}`
+    );
+    // Return sun - sat: 0 - 6
+    const dayOftheWeek = newDate.getDay();
+    const dayOftheWeekArray = [6, 0, 1, 2, 3, 4, 5];
+    // We want to return mon - sun: 0 -6
+    console.log(dayOftheWeekArray[dayOftheWeek]);
+    return dayOftheWeekArray[dayOftheWeek];
+  };
+
+  // Create all days array from provided month
+  const createDaysArray = (year, month, setter) => {
     const tempArray = [];
-    const days = new Date(props.calendarYear, props.calendarMonth, 0).getDate();
+    const days = new Date(year, month, 0).getDate();
     for (let i = 1; i <= days; i++) {
       tempArray.push(i);
     }
-    setDaysArray(tempArray);
+
+    setter(tempArray);
+  };
+
+  const dayClick = (item) => {
+    getDayOftheWeek(item);
   };
 
   useEffect(() => {
-    createDaysArray();
-  }, []);
+    createDaysArray(props.calendarYear, props.calendarMonth, setDaysArray);
+  }, [props.calendarMonth, props.calendarYear]);
 
   //
   return (
-    <div className="days-panel-warepper">
-      <div className="weekDays-wrapper">
+    <div className="calendar-days-panel-warepper">
+      <div className="calendar-weekDays-wrapper">
         {weekDays.map((item, index) => {
           return (
-            <div className="weekDays-card" key={index}>
-              <p>{item}</p>
+            <div className="calendar-weekDays-card" key={index}>
+              <p
+                style={{ color: item === "Sun" ? "red" : "black" }}
+                className="calendar-weekDay-nanme"
+              >
+                {item}
+              </p>
             </div>
           );
         })}
       </div>
-      <div className="days-wrapper">
+
+      <div className="calendar-days-wrapper">
         {daysArray.length > 0
           ? daysArray.map((item, index) => {
               return (
-                <div className="dayCard" key={index}>
-                  {item}
+                <div
+                  className="calendar-dayCard"
+                  key={index}
+                  onClick={() => dayClick(item)}
+                >
+                  <p className="calendar-weekDay-number">{item}</p>
                 </div>
               );
             })
---
+++ src/data/monthsWeekdays.js
@@ -0,0 +1,19 @@
+const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const months = [
+  "0",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export { weekDays, months };
